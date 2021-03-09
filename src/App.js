21b1546ca679e68c9149cd0d5b32dfcaaa524bed
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-09 13:51:59 +0600

    fetched using axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import News from './components/News/News';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
     const url = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b093438169e44148aa7051053a7a930e'
-    fetch(url)
-      .then(response => response.json())
-      .then(data => setArticles(data.articles))
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(data => setArticles(data.articles))
+    axios(url)
+    .then(data => setArticles(data.data.articles))
   },[])
   return (
     <div >
